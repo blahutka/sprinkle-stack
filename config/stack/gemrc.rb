d10e557a5a61d961a33475b935eb03b512cb6c70
Aditya Sanghi <asanghi@me.com>
2010-08-15 14:00:48 +0200

    adding new gemrc file

---
+++ config/stack/gemrc.rb
@@ -0,0 +1,17 @@
+package :gemrc do 
+  description "Supply gem sources, turn off ri and rdoc generation"
+
+  config =<<eol
+# gemrc for production
+---
+:sources:
+- http://gems.rubyforge.org
+- http://gems.github.com
+gem: --no-ri --no-rdoc
+eol
+  
+  push_text config, "~/.gemrc"
+  verify {file_contains "~/.gemrc", "gemrc for production"}
+
+end
+
