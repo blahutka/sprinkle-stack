8aa91b52f227842e9e36817682aa2b34b7ccdfac
Aditya Sanghi <asanghi@me.com>
2010-08-15 14:16:25 +0200

    adding fail2ban

--- config/stack/essential.rb
+++ config/stack/essential.rb
@@ -1,6 +1,6 @@
 package :build_essential do
   description 'Build tools'
-  apt 'build-essential' do
+  apt 'build-essential fail2ban' do
     pre :install, 'apt-get update'
     pre :install, 'apt-get upgrade'
   end
